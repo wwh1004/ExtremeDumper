863b217c08fc315ec4f614e9990305135c79dfd6
文煌 <wwh1004@hotmail.com>
2019-08-01 19:58:33 +0800

    clean code

--- ExtremeDumper/Forms/NativeMethods.cs
+++ ExtremeDumper/Forms/NativeMethods.cs
@@ -14,53 +14,35 @@ namespace ExtremeDumper.Forms {
 
 		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
 		public struct MODULEENTRY32 {
+			public static readonly uint UnmanagedSize = (uint)Marshal.SizeOf(typeof(MODULEENTRY32));
+			public static MODULEENTRY32 Default => new MODULEENTRY32 { dwSize = UnmanagedSize };
+
 			public uint dwSize;
-
 			public uint th32ModuleID;
-
 			public uint th32ProcessID;
-
 			public uint GlblcntUsage;
-
 			public uint ProccntUsage;
-
 			public IntPtr modBaseAddr;
-
 			public uint modBaseSize;
-
 			public IntPtr hModule;
-
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
 			public string szModule;
-
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
 			public string szExePath;
-
-			public static readonly uint UnmanagedSize = (uint)Marshal.SizeOf(typeof(MODULEENTRY32));
-
-			public static MODULEENTRY32 Default { get => new MODULEENTRY32 { dwSize = UnmanagedSize }; }
 		}
 
-		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-		public struct SCROLLBARINFO {
-			public uint cbSize;
-
-			public Rectangle rcScrollBar;
-
-			public int dxyLineButton;
-
-			public int xyThumbTop;
-
-			public int xyThumbBottom;
-
-			public int reserved;
-
-			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 6, ArraySubType = UnmanagedType.U4)]
-			public uint[] rgstate;
-
+		[StructLayout(LayoutKind.Sequential)]
+		public unsafe struct SCROLLBARINFO {
 			public static readonly uint UnmanagedSize = (uint)Marshal.SizeOf(typeof(SCROLLBARINFO));
+			public static SCROLLBARINFO Default = new SCROLLBARINFO { cbSize = UnmanagedSize };
 
-			public static SCROLLBARINFO Default { get => new SCROLLBARINFO { cbSize = UnmanagedSize }; }
+			public uint cbSize;
+			public Rectangle rcScrollBar;
+			public int dxyLineButton;
+			public int xyThumbTop;
+			public int xyThumbBottom;
+			public int reserved;
+			public fixed uint rgstate[6];
 		}
 
 		[DllImport("kernel32.dll", BestFitMapping = false, CharSet = CharSet.Unicode, EntryPoint = "CreateToolhelp32Snapshot", ExactSpelling = true, SetLastError = true)]
