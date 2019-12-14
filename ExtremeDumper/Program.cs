cef0c7280369539ff359876ea1e3edacc208a8f9
文煌 <wwh1004@hotmail.com>
2019-12-14 14:02:22 +0800

    修复编译错误 修复搜索符合文件导致卡死 更新子模块

--- ExtremeDumper/Program.cs
+++ ExtremeDumper/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Windows.Forms;
 using ExtremeDumper.Forms;
+using Microsoft.Diagnostics.Runtime.Utilities;
 
 namespace ExtremeDumper {
 	public static class Program {
 		[STAThread]
 		public static void Main() {
+			SymbolLocator._NT_SYMBOL_PATH = " ";
+			// 禁止在线搜索PDB文件
 			GlobalExceptionCatcher.Catch();
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
