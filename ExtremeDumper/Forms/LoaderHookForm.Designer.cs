b4ddba0cd3f8884804f5dc88da5dc998e33ebf7b
wwh1004 <wwh1004@hotmail.com>
2022-03-12 13:01:32 +0800

    add LoaderHookForm

--- ExtremeDumper/Forms/LoaderHookForm.Designer.cs
+++ ExtremeDumper/Forms/LoaderHookForm.Designer.cs
@@ -24,37 +24,56 @@ partial class LoaderHookForm {
 	/// the contents of this method with the code editor.
 	/// </summary>
 	private void InitializeComponent() {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btSelectAssembly = new System.Windows.Forms.Button();
+            this.tbAssemblyPath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.odlgSelectAssembly = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // textBox1
+            // btSelectAssembly
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(619, 27);
-            this.textBox1.TabIndex = 0;
+            this.btSelectAssembly.Location = new System.Drawing.Point(12, 47);
+            this.btSelectAssembly.Name = "btSelectAssembly";
+            this.btSelectAssembly.Size = new System.Drawing.Size(172, 27);
+            this.btSelectAssembly.TabIndex = 9;
+            this.btSelectAssembly.Text = "Select Assembly...";
+            this.btSelectAssembly.UseVisualStyleBackColor = true;
+            this.btSelectAssembly.Click += new System.EventHandler(this.btSelectAssembly_Click);
             // 
-            // button1
+            // tbAssemblyPath
             // 
-            this.button1.Location = new System.Drawing.Point(637, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 27);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Browse file...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.tbAssemblyPath.Location = new System.Drawing.Point(12, 13);
+            this.tbAssemblyPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbAssemblyPath.Name = "tbAssemblyPath";
+            this.tbAssemblyPath.Size = new System.Drawing.Size(856, 27);
+            this.tbAssemblyPath.TabIndex = 8;
+            this.tbAssemblyPath.TextChanged += new System.EventHandler(this.tbAssemblyPath_TextChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(455, 47);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(172, 27);
+            this.btnRun.TabIndex = 10;
+            this.btnRun.Text = "Run With Hook";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
             // 
             // LoaderHookForm
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(938, 245);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(878, 86);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btSelectAssembly);
+            this.Controls.Add(this.tbAssemblyPath);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
             this.Icon = global::ExtremeDumper.Forms.Resources.Icon;
             this.Name = "LoaderHookForm";
             this.Text = "Loader Hook";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.LoaderHookForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.LoaderHookForm_DragEnter);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,6 +81,8 @@ partial class LoaderHookForm {
 
 	#endregion
 
-	private System.Windows.Forms.TextBox textBox1;
-	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.Button btSelectAssembly;
+	private System.Windows.Forms.TextBox tbAssemblyPath;
+	private System.Windows.Forms.Button btnRun;
+	private System.Windows.Forms.OpenFileDialog odlgSelectAssembly;
 }
--- ExtremeDumper/Forms/LoaderHookForm.cs
+++ ExtremeDumper/Forms/LoaderHookForm.cs
@@ -1,9 +1,46 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 
 namespace ExtremeDumper.Forms;
 
 partial class LoaderHookForm : Form {
+	[DllImport("ExtremeDumper.LoaderHook.dll", BestFitMapping = false, CharSet = CharSet.Unicode, SetLastError = true)]
+	static extern uint LoaderHookCreateProcess(string applicationName, StringBuilder? commandLine);
+
+	string assemblyPath = string.Empty;
+
 	public LoaderHookForm() {
 		InitializeComponent();
 	}
+
+	#region Events
+	void LoaderHookForm_DragEnter(object sender, DragEventArgs e) {
+		e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+	}
+
+	void LoaderHookForm_DragDrop(object sender, DragEventArgs e) {
+		tbAssemblyPath.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+	}
+
+	void tbAssemblyPath_TextChanged(object sender, EventArgs e) {
+		assemblyPath = tbAssemblyPath.Text;
+	}
+
+	void btSelectAssembly_Click(object sender, EventArgs e) {
+		if (odlgSelectAssembly.ShowDialog() == DialogResult.OK)
+			tbAssemblyPath.Text = odlgSelectAssembly.FileName;
+		else
+			return;
+	}
+
+	void btnRun_Click(object sender, EventArgs e) {
+		if (string.IsNullOrEmpty(assemblyPath))
+			return;
+
+		uint hr = LoaderHookCreateProcess(assemblyPath, null);
+		MessageBoxStub.Show(hr == 0 ? "Succeed" : $"Failed, try ExtremeDumper{(IntPtr.Size == 4 ? string.Empty : "-x86")}.exe", hr == 0 ? MessageBoxIcon.Information : MessageBoxIcon.Error);
+	}
+	#endregion
 }
